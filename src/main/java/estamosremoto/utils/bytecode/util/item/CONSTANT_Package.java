9c423fbf678730fa009aa289c9b967049f56d176
Bautista Bambozzi <bautistabambozzi@gmail.com>
2024-05-04 19:50:17 -0300

    add module and package

---
+++ src/main/java/estamosremoto/utils/bytecode/util/item/CONSTANT_Package.java
@@ -0,0 +1,13 @@
+package estamosremoto.utils.bytecode.util.item;
+
+import estamosremoto.utils.bytechannel.ByteChannelParser;
+import estamosremoto.utils.bytecode.util.tag.ConstantPoolTag;
+
+import java.nio.channels.ByteChannel;
+
+public record CONSTANT_Package(ConstantPoolTag tag, int name_index) implements ConstantPoolItem {
+
+    public CONSTANT_Package(ByteChannel channel) {
+        this(ConstantPoolTag.CONSTANT_Package, ByteChannelParser.parseU2(channel));
+    }
+}
