0b548d2a2811641ef6400e2680efe5094cce6931
Bautista Bambozzi <bautistabambozzi@gmail.com>
2024-05-05 00:21:57 -0300

    Add access flags!

---
+++ src/main/java/estamosremoto/utils/bytecode/util/accessflag/AccessFlag.java
@@ -0,0 +1,40 @@
+package estamosremoto.utils.bytecode.util.accessflag;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public enum AccessFlag {
+    ACC_PUBLIC(0x0001),
+    ACC_FINAL(0x0010),
+    ACC_SUPER(0x0020),
+    ACC_INTERFACE(0x0200),
+    ACC_ABSTRACT(0x0400),
+    ACC_SYNTHETIC(0x1000),
+    ACC_ANNOTATION(0x2000),
+    ACC_ENUM(0x4000),
+    ACC_MODULE(0x8000);
+    private static final Map<Integer, AccessFlag> map;
+
+    static {
+        Map<Integer, AccessFlag> elements = new HashMap<>();
+        for (AccessFlag flag : AccessFlag.values()) {
+            elements.put(flag.value, flag);
+        }
+        map = Collections.unmodifiableMap(elements);
+    }
+
+    private final int value;
+
+    AccessFlag(int value) {
+        this.value = value;
+    }
+
+    public AccessFlag parse(int value) {
+        AccessFlag flag = map.get(value);
+        if (flag == null) {
+            throw new IllegalArgumentException("Invalid access flag: " + value);
+        }
+        return flag;
+    }
+}
