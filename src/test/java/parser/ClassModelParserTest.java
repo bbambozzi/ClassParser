926cd16b8563aec37329d1ff8d852e9bf66a55f1
Bautista Bambozzi <bautistabambozzi@gmail.com>
2024-05-07 16:08:30 -0300

    Added Maven surefire and initial tests

--- src/test/java/parser/ClassModelParserTest.java
+++ src/test/java/parser/ClassModelParserTest.java
@@ -1,5 +1,29 @@
 package parser;
 
-@Test
+import bbambozzi.parser.ClassModelParser;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Path;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ClassModelParserTest {
+
+    @Test()
+    void testsLoad() {
+        assertEquals(0, 0);
+    }
+    @Test
+    void resourcesExist() {
+        Path path = Path.of("src/main/resources/examples/single-file/HelloWorld.class");
+        assertNotNull(path);
+    }
+
+    @Test
+    void classModelParserLoads() {
+        Path path = Path.of("src/main/resources/examples/single-file/HelloWorld.class");
+        ClassModelParser parser = new ClassModelParser(path);
+        assertNotNull(parser);
+    }
+
 }
